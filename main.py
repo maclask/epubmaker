e21e7ad40973359a5f1217eb707cc2ffa405120e
Maciej Laskowski <laskowski.maciej@outlook.com>
2019-04-04 21:33:33 +0200

    Add function for matching images

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import difflib
+from PIL import Image
+from resizeimage import resizeimage
+import re
 
 try:
     from xml.etree.cElementTree import XML
@@ -7,6 +10,15 @@ except ImportError:
 import zipfile
 import io
 import os
+from enum import Enum
+
+
+class Type(Enum):
+    PARAGRAPH = 0
+    BOLD = 1
+    ITALIC = 2
+
+
 """
 Module that extract text from MS XML Word document (.docx).
 (Inspired by python-docx <https://github.com/mikemaccana/python-docx>)
@@ -19,47 +31,64 @@ TEXT_SECTION = WORD_NAMESPACE + 't'
 BOLD = WORD_NAMESPACE + 'b'
 ITALIC = WORD_NAMESPACE + 'i'
 RPR = WORD_NAMESPACE + 'rPr'
+BR = WORD_NAMESPACE + 'br'
 ARTICLES_DIR = ''
 GRAPHICS_DIR = ''
+IMAGES_DIR = ''
 GRAPHICS_MATCH_WORD = 'TYTULOWA'
+title = ''
 
-def get_docx_text(path):
+
+def get_docx_text(path, filename):
     """
     Take the path of a docx file as argument, return the text in unicode.
     """
-    document = zipfile.ZipFile(path)
+    document = zipfile.ZipFile(path + filename)
     xml_content = document.read('word/document.xml')
     document.close()
     tree = XML(xml_content)
 
     paragraphs = []
     texts = []
-    iterator = 0 #0 nagłówek, 1 autor, 2 lead
-    typ = 0 #0 akapit, 1 bold, 2 kursywa
+    iterator = 0
     for paragraph in tree.iter(PARA):
         for node in paragraph.iter(TEXT):
+
             if node.findall(TEXT_SECTION):
                 if node.find(RPR) and node.find(RPR).findall(BOLD):
-                    typ = 1
+                    paragraph_type = Type.BOLD
                 elif node.find(RPR) and node.find(RPR).findall(ITALIC):
-                    typ = 2
+                    texts.append('<i>' + ''.join(node.find(TEXT_SECTION).text) + '</i>')
+                    continue
+                    paragraph_type = Type.ITALIC
                 else:
-                    typ = 0
+                    paragraph_type = Type.PARAGRAPH
 
-                texts.append(node.find(TEXT_SECTION).text)
+                all_text = node.findall(TEXT_SECTION)
+                for textes in all_text:
+                    texts.append("".join(textes.itertext()))
         if texts:
-            if typ == 1 and iterator > 2:
+            if paragraph_type == Type.BOLD and iterator > 2:
                 paragraphs.append('<h2>' + ''.join(texts) + '</h2>')
             elif iterator == 0:
-                paragraphs.append('<h1>'+''.join(texts)+'</h1>')
+                global title
+                title = ''.join(texts)
+                paragraphs.append('<h1>' + ''.join(texts) + '</h1><hr />')
             elif iterator == 1:
                 paragraphs.append('<p class="author">' + ''.join(texts) + '</p>')
-            elif typ == 1 and iterator == 2:
+                image = addimage(filename)
+                #image = ''
+                if image:
+                    resizeimg(IMAGES_DIR + image)
+                    paragraphs.append('<img src="../Images/' + image + '">')
+                    m = re.search("\[(.+?)\]", image)
+                    if m:
+                        paragraphs.append('<p class="credit">' + m.group(1) + '</p>')
+            elif paragraph_type == Type.BOLD and iterator == 2:
                 paragraphs.append('<p class="lead">' + ''.join(texts) + '</p>')
-
-            elif texts and typ == 2:
+            elif texts and paragraph_type == Type.ITALIC:
                 paragraphs.append('<i>' + ''.join(texts) + '</i>')
-            elif texts and typ == 0:
+            elif texts and paragraph_type == Type.PARAGRAPH:
                 paragraphs.append('<p>' + ''.join(texts) + '</p>')
             iterator += 1
         texts = []
@@ -73,17 +102,15 @@ def run():
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         if filename.endswith(".docx") or filename.endswith(".doc"):
-            a = get_docx_text(dirstr+filename)
-            image = match(filename)
-            a = a + '<img src="' + image + '">'
-            with io.open(dirstr+'gotowe/'+filename+'.html', "w", encoding="utf-8") as f:
+            a = get_docx_text(dirstr, filename)
+            with io.open(dirstr + 'gotowe/' + filename + '.html', "w", encoding="utf-8") as f:
                 f.write(a)
         else:
             continue
     # print(a)
 
 
-def match(article):
+def addimage(article):
     dirstr = GRAPHICS_DIR
     directory = os.fsencode(dirstr)
     graphics = [os.fsdecode(file)
@@ -95,12 +122,21 @@ def match(article):
     imageslist = [os.fsdecode(file)
                   for file in os.listdir(directory)]
     matchimageslist = difflib.get_close_matches(GRAPHICS_MATCH_WORD, imageslist, 1, 0.3)
+
     print(matchimageslist)
     if len(matchimageslist) > 0:
+        os.rename(GRAPHICS_DIR + folderlist[0] + '/' + matchimageslist[0],
+                  IMAGES_DIR + matchimageslist[0])
         return matchimageslist[0]
     else:
         return ""
 
+
+def resizeimg(path):
+    with open(path, 'r+b') as f:
+        with Image.open(f) as image:
+            cover = resizeimage.resize_cover(image, [600, 400])
+            cover.save(path, image.format)
 
 
 run()
